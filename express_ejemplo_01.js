aff9fb628998fe267eeb5deb05749544ae3870a0
Juan Carlos Cuevas Martínez <jccuevas@users.noreply.github.com>
2025-04-01 12:10:17 +0200

    Express - Ejemplo sencillo de métodos y rutas

---
+++ express_ejemplo_01.js
@@ -0,0 +1,31 @@
+const express = require("express"); // Se carga el módulo Express
+const app = express(); // Se crea la instancia de Express que permitirá acceder a toda su funcionalidad.
+
+//Responda con Hello World! en la página inicial:
+app.get("/", function (req, res) {
+  console.log("Petición GET recibida...");
+  res.send("Hello World!");
+});
+
+//Responda a la solicitud POST en la ruta raíz (/), la página de inicio de la aplicación:
+app.post("/", function (req, res) {
+  console.log("Petición POST recibida...");
+  res.send("Got a POST request");
+});
+
+//Responda a una solicitud PUT en la ruta /user:
+app.put("/user", function (req, res) {
+  console.log("Petición PUT recibida...");
+  res.send("Got a PUT request at /user");
+});
+
+//Responda a una solicitud DELETE en la ruta /user:
+app.delete("/user", function (req, res) {
+  console.log("Petición DELETE recibida...");
+  res.send("Got a DELETE request at /user");
+});
+
+// Inicia el servidor en la dirección de loopback y puerto 3000
+app.listen(3000, function () {
+  console.log("Ejemplo de app escuchando en el puerto 3000");
+});
